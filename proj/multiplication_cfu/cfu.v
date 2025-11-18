ed7bc47b132cfc62d07158d4bbe0f288f9206fe1
Julius Friesen <julius.friesen.26@outlook.com>
2025-11-18 07:35:59 +0100

    Switched from Amaranth to Verilog Flow

---
+++ proj/multiplication_cfu/cfu.v
@@ -0,0 +1,33 @@
+module Cfu (
+  input               cmd_valid,
+  output              cmd_ready,
+  input      [9:0]    cmd_payload_function_id,
+  input      [31:0]   cmd_payload_inputs_0,
+  input      [31:0]   cmd_payload_inputs_1,
+  output reg          rsp_valid,
+  input               rsp_ready,
+  output reg [31:0]   rsp_payload_outputs_0,
+  input               reset,
+  input               clk
+);
+  assign cmd_ready = ~rsp_valid;
+
+  always @(posedge clk) begin
+    if (reset) begin
+      rsp_payload_outputs_0 <= 32'b0;
+      rsp_valid <= 1'b0;
+    end else if (rsp_valid) begin
+      // Waiting to hand off response to CPU.
+      rsp_valid <= ~rsp_ready;
+    end else if (cmd_valid) begin
+      rsp_valid <= 1'b1;
+      // Calculation step
+      case (cmd_payload_function_id)
+        10'd0: rsp_payload_outputs_0 <= cmd_payload_inputs_0 + cmd_payload_inputs_1; // Add
+        10'd1: rsp_payload_outputs_0 <= cmd_payload_inputs_0 - cmd_payload_inputs_1; // Sub
+        10'd2: rsp_payload_outputs_0 <= cmd_payload_inputs_0 * cmd_payload_inputs_1; // Mul
+        default: rsp_payload_outputs_0 <= 0; // Not defined
+      endcase
+    end
+  end
+endmodule
