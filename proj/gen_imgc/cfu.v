b5e146c39e07f50fda2b289856a6eb00e0038a3e
Julius Friesen <julius.friesen.26@outlook.com>
2026-03-25 01:08:33 +0100

    IMGC: Initial

---
+++ proj/gen_imgc/cfu.v
@@ -0,0 +1,29 @@
+module Cfu (
+  input               cmd_valid,
+  output              cmd_ready,
+  input      [9:0]    cmd_payload_function_id,
+  input      [31:0]   cmd_payload_inputs_0,
+  input      [31:0]   cmd_payload_inputs_1,
+  output reg          rsp_valid,
+  input               rsp_ready,
+  output reg [31:0]   rsp_payload_outputs_0,
+  input               reset,
+  input               clk
+);
+
+  // Only not ready for a command when we have a response.
+  assign cmd_ready = ~rsp_valid;
+
+  always @(posedge clk) begin
+    if (reset) begin
+      rsp_payload_outputs_0 <= 32'b0;
+      rsp_valid <= 1'b0;
+    end else if (rsp_valid) begin
+      // Waiting to hand off response to CPU.
+      rsp_valid <= ~rsp_ready;
+    end else if (cmd_valid) begin
+      rsp_valid <= 1'b1;
+      rsp_payload_outputs_0 <= 32'b0;
+    end
+  end
+endmodule
