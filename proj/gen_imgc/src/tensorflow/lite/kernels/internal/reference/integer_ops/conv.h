02686f02f5e769deb0d80c963b7727c4859232db
Julius Friesen <julius.friesen.26@outlook.com>
2026-03-25 01:18:34 +0100

    Hardcode input_offset and output_activation (min/max), remove dilation and groups

--- proj/gen_imgc/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
+++ proj/gen_imgc/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
@@ -32,18 +32,16 @@ inline void ConvPerChannel(
     const int32_t* bias_data, const RuntimeShape& output_shape,
     int8_t* output_data) {
   // Get parameters.
-  const int32_t input_offset = params.input_offset;  // r = s(q - Z)
+  const int32_t input_offset = 128;  // r = s(q - Z)
   const int stride_width = params.stride_width;
   const int stride_height = params.stride_height;
-  const int dilation_width_factor = params.dilation_width_factor;
-  const int dilation_height_factor = params.dilation_height_factor;
   const int pad_width = params.padding_values.width;
   const int pad_height = params.padding_values.height;
   const int32_t output_offset = params.output_offset;
 
   // Set min and max value of the output.
-  const int32_t output_activation_min = params.quantized_activation_min;
-  const int32_t output_activation_max = params.quantized_activation_max;
+  const int32_t output_activation_min = -128;
+  const int32_t output_activation_max = 127;
 
   // Consistency check.
   TFLITE_DCHECK_LE(output_activation_min, output_activation_max);
@@ -63,9 +61,7 @@ inline void ConvPerChannel(
   const int filter_height = filter_shape.Dims(1);
   const int filter_width = filter_shape.Dims(2);
   const int filter_input_depth = filter_shape.Dims(3);
-  const int groups = input_depth / filter_input_depth;
   TFLITE_DCHECK_EQ(input_depth % filter_input_depth, 0);
-  const int filters_per_group = output_depth / groups;
   const int output_height = output_shape.Dims(1);
   const int output_width = output_shape.Dims(2);
   for (int batch = 0; batch < batches; ++batch) {
@@ -74,12 +70,11 @@ inline void ConvPerChannel(
       for (int out_x = 0; out_x < output_width; ++out_x) {
         const int in_x_origin = (out_x * stride_width) - pad_width;
         for (int out_channel = 0; out_channel < output_depth; ++out_channel) {
-          auto group = out_channel / filters_per_group;
           int32_t acc = 0;
           for (int filter_y = 0; filter_y < filter_height; ++filter_y) {
-            const int in_y = in_y_origin + dilation_height_factor * filter_y;
+            const int in_y = in_y_origin + filter_y;
             for (int filter_x = 0; filter_x < filter_width; ++filter_x) {
-              const int in_x = in_x_origin + dilation_width_factor * filter_x;
+              const int in_x = in_x_origin + filter_x;
 
               // Zero padding by omitting the areas outside the image.
               const bool is_point_inside_image =
@@ -94,7 +89,7 @@ inline void ConvPerChannel(
                    ++in_channel) {
                 int32_t input_val =
                     input_data[Offset(input_shape, batch, in_y, in_x,
-                                      in_channel + group * filter_input_depth)];
+                                      in_channel)];
                 int32_t filter_val = filter_data[Offset(
                     filter_shape, out_channel, filter_y, filter_x, in_channel)];
                 
