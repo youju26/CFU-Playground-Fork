03eb3bf243d0c7f7769f642258c122e765e1b222
Julius Friesen <julius.friesen.26@outlook.com>
2026-03-31 13:20:18 +0200

    ADTI8: Hardcoded batches, output_activation min and max, and removed weight_offset related code

--- proj/gen_adti8/src/tensorflow/lite/kernels/internal/reference/integer_ops/fully_connected.h
+++ proj/gen_adti8/src/tensorflow/lite/kernels/internal/reference/integer_ops/fully_connected.h
@@ -118,19 +118,18 @@ inline void FullyConnected(
     const int32_t* bias_data, const RuntimeShape& output_shape,
     int8_t* output_data) {
   const int32_t input_offset = params.input_offset;
-  const int32_t filter_offset = params.weights_offset;
   const int32_t output_offset = params.output_offset;
   const int32_t output_multiplier = params.output_multiplier;
   const int output_shift = params.output_shift;
-  const int32_t output_activation_min = params.quantized_activation_min;
-  const int32_t output_activation_max = params.quantized_activation_max;
+  const int32_t output_activation_min = -128;
+  const int32_t output_activation_max = 127;
   TFLITE_DCHECK_GE(filter_shape.DimensionsCount(), 2);
   TFLITE_DCHECK_GE(output_shape.DimensionsCount(), 1);
 
   TFLITE_DCHECK_LE(output_activation_min, output_activation_max);
   const int filter_dim_count = filter_shape.DimensionsCount();
   const int output_dim_count = output_shape.DimensionsCount();
-  const int batches = FlatSizeSkipDim(output_shape, output_dim_count - 1);
+  const int batches = 1;
   const int output_depth = output_shape.Dims(output_dim_count - 1);
   TFLITE_DCHECK_LE(output_depth, filter_shape.Dims(filter_dim_count - 2));
   const int accum_depth = filter_shape.Dims(filter_dim_count - 1);
@@ -140,7 +139,7 @@ inline void FullyConnected(
       for (int d = 0; d < accum_depth; ++d) {
         int32_t input_val = input_data[b * accum_depth + d];
         int32_t filter_val = filter_data[out_c * accum_depth + d];
-        acc += (filter_val + filter_offset) * (input_val + input_offset);
+        acc += (filter_val) * (input_val + input_offset);
       }
       if (bias_data) {
         acc += bias_data[out_c];
