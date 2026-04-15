687715b91e60246b4a5233de344e5d869f51fe2e
Julius Friesen <julius.friesen.26@outlook.com>
2026-04-15 04:03:47 +0200

    VWW: Input Buffer Implementation

--- proj/gen_vww/cfu.v
+++ proj/gen_vww/cfu.v
@@ -1,5 +1,6 @@
 `include "gateware/cfu_types.vh"
 `include "gateware/cfu_mac.v"
+`include "gateware/cfu_input_buffer.v"
 
 module Cfu (
   input               cmd_valid,
@@ -14,6 +15,42 @@ module Cfu (
   input               clk
 );
 
+  // <--- Buffer for Input Vals --->
+  reg flag_buffer_clear;
+  reg flag_buffer_write_en;
+  reg flag_buffer_read_en;
+  
+  wire flag_buffer_read_valid;
+  wire [8:0] buffer_count;
+  wire buffer_write_full;
+  wire buffer_read_empty;
+
+  reg signed [31:0] buffer_write_data;
+  wire signed [31:0] buffer_read_data;
+  wire signed [8:0] input_0_and_offset;
+  wire signed [8:0] input_1_and_offset;
+  wire signed [8:0] input_2_and_offset;
+  wire signed [8:0] input_3_and_offset;
+
+  cfu_input_buffer u_input_buffer (
+    .clk(clk),
+    .rst(reset),
+    .clear(flag_buffer_clear),
+    .write_en(flag_buffer_write_en),
+    .write_data(buffer_write_data),
+    .write_full(buffer_write_full),
+    .read_en(flag_buffer_read_en),
+    .read_data(buffer_read_data),
+    .offset(mac_offset),
+    .input_0_and_offset(input_0_and_offset),
+    .input_1_and_offset(input_1_and_offset),
+    .input_2_and_offset(input_2_and_offset),
+    .input_3_and_offset(input_3_and_offset),
+    .read_data_valid(flag_buffer_read_valid),
+    .read_empty(buffer_read_empty),
+    .count(buffer_count)
+  );
+
   // <--- MAC --->
   wire signed [15:0] prod_0;
   wire signed [15:0] prod_1;
@@ -25,10 +62,12 @@ module Cfu (
   reg signed [31:0] mac_acc_2;
   reg signed [31:0] mac_acc_3;
 
-  cfu_mac u_mac(
+  cfu_mac u_mac_buffer(
     .a(cmd_payload_inputs_0),  // Weights
-    .b(cmd_payload_inputs_1),  // Activations
-    .offset(mac_offset),
+    .input_0_and_offset(input_0_and_offset),
+    .input_1_and_offset(input_1_and_offset),
+    .input_2_and_offset(input_2_and_offset),
+    .input_3_and_offset(input_3_and_offset),
     .prod_0(prod_0),
     .prod_1(prod_1),
     .prod_2(prod_2),
@@ -39,6 +78,11 @@ module Cfu (
   assign cmd_ready = ~rsp_valid; // Ready to receive new command if no pending output
 
   always @(posedge clk) begin
+
+    // Default: 
+    flag_buffer_clear <= 1'b0;
+    flag_buffer_read_en <= 1'b0;
+    flag_buffer_write_en <= 1'b0;
 
     if (reset) begin
       rsp_valid <= 1'b0; // Output not valid
@@ -62,13 +106,6 @@ module Cfu (
       case (cmd_payload_function_id[2:0])
         `MAC: begin
           case (cmd_payload_function_id[9:3])
-            `MAC_ACC: begin
-              // Add MAC sum to each acc
-              mac_acc_0 <= mac_acc_0 + prod_0;
-              mac_acc_1 <= mac_acc_1 + prod_1;
-              mac_acc_2 <= mac_acc_2 + prod_2;
-              mac_acc_3 <= mac_acc_3 + prod_3;
-            end
             `MAC_CLEAR: begin
               mac_acc_0 <= 32'd0;
               mac_acc_1 <= 32'd0;
@@ -80,6 +117,24 @@ module Cfu (
             end
             `MAC_GET: begin
               rsp_payload_outputs_0 <= mac_acc_0 + mac_acc_1 + mac_acc_2 + mac_acc_3;
+            end
+            `MAC_SET_INPUT_VALS: begin
+              buffer_write_data <= cmd_payload_inputs_0;
+              flag_buffer_write_en <= 1'b1;
+            end
+            `MAC_ON_BUFFER: begin             
+              flag_buffer_read_en <= 1'b1;
+              // Add MAC sum to each acc
+              mac_acc_0 <= mac_acc_0 + prod_0;
+              mac_acc_1 <= mac_acc_1 + prod_1;
+              mac_acc_2 <= mac_acc_2 + prod_2;
+              mac_acc_3 <= mac_acc_3 + prod_3;
+              // Re-append the value to keep it in the buffer for multiple filter sets
+              flag_buffer_write_en <= 1'b1;
+              buffer_write_data <= buffer_read_data;
+            end
+            `MAC_CLEAR_INPUT_VALS: begin
+              flag_buffer_clear <= 1'b1;
             end
           endcase
         end
