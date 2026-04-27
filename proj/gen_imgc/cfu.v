14191b7a1ecbc1a08004f6d34677f77d1558277e
Julius Friesen <julius.friesen.26@outlook.com>
2026-04-27 17:52:04 +0200

    IMGC: Filter Buffer

--- proj/gen_imgc/cfu.v
+++ proj/gen_imgc/cfu.v
@@ -2,6 +2,7 @@
 `include "gateware/cfu_mac.v"
 `include "gateware/cfu_input_buffer.v"
 `include "gateware/cfu_quantizer.v"
+`include "gateware/cfu_filter_buffer.v"
 
 module Cfu (
   input               cmd_valid,
@@ -15,123 +16,6 @@ module Cfu (
   input               reset,
   input               clk
 );
-
-  // <--- Buffer for Input Vals --->
-  reg flag_buffer_clear;
-  reg flag_buffer_write_en;
-  reg flag_buffer_read_en;
-  
-  wire flag_buffer_read_valid;
-  wire [8:0] buffer_count;
-  wire buffer_write_full;
-  wire buffer_read_empty;
-
-  reg signed [31:0] buffer_write_data;
-  wire signed [31:0] buffer_read_data;
-  wire signed [8:0] input_0_and_offset;
-  wire signed [8:0] input_1_and_offset;
-  wire signed [8:0] input_2_and_offset;
-  wire signed [8:0] input_3_and_offset;
-
-  cfu_input_buffer u_input_buffer (
-    .clk(clk),
-    .rst(reset),
-    .clear(flag_buffer_clear),
-    .write_en(flag_buffer_write_en),
-    .write_data(buffer_write_data),
-    .write_full(buffer_write_full),
-    .read_en(flag_buffer_read_en),
-    .read_data(buffer_read_data),
-    .offset(mac_offset),
-    .input_0_and_offset(input_0_and_offset),
-    .input_1_and_offset(input_1_and_offset),
-    .input_2_and_offset(input_2_and_offset),
-    .input_3_and_offset(input_3_and_offset),
-    .read_data_valid(flag_buffer_read_valid),
-    .read_empty(buffer_read_empty),
-    .count(buffer_count)
-  );
-
-  // <--- Buffer B for Input Vals --->
-  reg flag_buffer_clear_b;
-  reg flag_buffer_write_en_b;
-  reg flag_buffer_read_en_b;
-  
-  wire flag_buffer_read_valid_b;
-  wire [8:0] buffer_count_b;
-  wire buffer_write_full_b;
-  wire buffer_read_empty_b;
-
-  reg signed [31:0] buffer_write_data_b;
-  wire signed [31:0] buffer_read_data_b;
-  wire signed [8:0] input_0_and_offset_b;
-  wire signed [8:0] input_1_and_offset_b;
-  wire signed [8:0] input_2_and_offset_b;
-  wire signed [8:0] input_3_and_offset_b;
-
-  cfu_input_buffer u_input_buffer_b (
-    .clk(clk),
-    .rst(reset),
-    .clear(flag_buffer_clear_b),
-    .write_en(flag_buffer_write_en_b),
-    .write_data(buffer_write_data_b),
-    .write_full(buffer_write_full_b),
-    .read_en(flag_buffer_read_en_b),
-    .read_data(buffer_read_data_b),
-    .offset(mac_offset),
-    .input_0_and_offset(input_0_and_offset_b),
-    .input_1_and_offset(input_1_and_offset_b),
-    .input_2_and_offset(input_2_and_offset_b),
-    .input_3_and_offset(input_3_and_offset_b),
-    .read_data_valid(flag_buffer_read_valid_b),
-    .read_empty(buffer_read_empty_b),
-    .count(buffer_count_b)
-  );
-
-  // <--- MAC --->
-  wire signed [15:0] prod_0;
-  wire signed [15:0] prod_1;
-  wire signed [15:0] prod_2;
-  wire signed [15:0] prod_3;
-  reg signed [8:0] mac_offset;
-  reg signed [31:0] mac_acc_0;
-  reg signed [31:0] mac_acc_1;
-  reg signed [31:0] mac_acc_2;
-  reg signed [31:0] mac_acc_3;
-
-  cfu_mac u_mac_buffer(
-    .a(cmd_payload_inputs_0),  // Weights
-    .input_0_and_offset(input_0_and_offset),
-    .input_1_and_offset(input_1_and_offset),
-    .input_2_and_offset(input_2_and_offset),
-    .input_3_and_offset(input_3_and_offset),
-    .prod_0(prod_0),
-    .prod_1(prod_1),
-    .prod_2(prod_2),
-    .prod_3(prod_3)
-  );
-
-    // <--- MAC B--->
-  wire signed [15:0] prod_0_b;
-  wire signed [15:0] prod_1_b;
-  wire signed [15:0] prod_2_b;
-  wire signed [15:0] prod_3_b;
-  //reg signed [31:0] mac_acc_0;
-  //reg signed [31:0] mac_acc_1;
-  //reg signed [31:0] mac_acc_2;
-  //reg signed [31:0] mac_acc_3;
-
-  cfu_mac u_mac_buffer_b(
-    .a(cmd_payload_inputs_1),  // Weights
-    .input_0_and_offset(input_0_and_offset_b),
-    .input_1_and_offset(input_1_and_offset_b),
-    .input_2_and_offset(input_2_and_offset_b),
-    .input_3_and_offset(input_3_and_offset_b),
-    .prod_0(prod_0_b),
-    .prod_1(prod_1_b),
-    .prod_2(prod_2_b),
-    .prod_3(prod_3_b)
-  );
 
   // <--- Quantizer --->
   reg signed [31:0] qnt_bias;
@@ -160,18 +44,219 @@ module Cfu (
     .status(qnt_status)
   );
 
+  localparam integer FILTER_BANKS = 32;
+  localparam integer FILTER_BANK_ADDR_W = 5;
+
+  // <--- Buffer for Input Vals --->
+  reg flag_input_buffer_clear;
+  reg flag_input_buffer_write_en;
+  reg flag_input_buffer_read_en;
+  reg signed [8:0] mac_offset;
+  
+  wire flag_input_buffer_read_valid;
+  wire [8:0] input_buffer_count;
+  wire input_buffer_write_full;
+  wire input_buffer_read_empty;
+
+  reg signed [31:0] input_buffer_write_data;
+  wire signed [31:0] input_buffer_read_data;
+  wire signed [8:0] input_0_and_offset;
+  wire signed [8:0] input_1_and_offset;
+  wire signed [8:0] input_2_and_offset;
+  wire signed [8:0] input_3_and_offset;
+
+  cfu_input_buffer u_input_buffer (
+    .clk(clk),
+    .rst(reset),
+    .clear(flag_input_buffer_clear),
+    .write_en(flag_input_buffer_write_en),
+    .write_data(input_buffer_write_data),
+    .write_full(input_buffer_write_full),
+    .read_en(flag_input_buffer_read_en),
+    .read_data(input_buffer_read_data),
+    .offset(mac_offset),
+    .input_0_and_offset(input_0_and_offset),
+    .input_1_and_offset(input_1_and_offset),
+    .input_2_and_offset(input_2_and_offset),
+    .input_3_and_offset(input_3_and_offset),
+    .read_data_valid(flag_input_buffer_read_valid),
+    .read_empty(input_buffer_read_empty),
+    .count(input_buffer_count)
+  );
+
+  // <--- Buffer B for Input Vals --->
+  reg flag_input_buffer_clear_b;
+  reg flag_input_buffer_write_en_b;
+  reg flag_input_buffer_read_en_b;
+
+  wire flag_input_buffer_read_valid_b;
+  wire [8:0] input_buffer_count_b;
+  wire input_buffer_write_full_b;
+  wire input_buffer_read_empty_b;
+
+  reg signed [31:0] input_buffer_write_data_b;
+  wire signed [31:0] input_buffer_read_data_b;
+  wire signed [8:0] input_0_and_offset_b;
+  wire signed [8:0] input_1_and_offset_b;
+  wire signed [8:0] input_2_and_offset_b;
+  wire signed [8:0] input_3_and_offset_b;
+
+  cfu_input_buffer u_input_buffer_b (
+    .clk(clk),
+    .rst(reset),
+    .clear(flag_input_buffer_clear_b),
+    .write_en(flag_input_buffer_write_en_b),
+    .write_data(input_buffer_write_data_b),
+    .write_full(input_buffer_write_full_b),
+    .read_en(flag_input_buffer_read_en_b),
+    .read_data(input_buffer_read_data_b),
+    .offset(mac_offset),
+    .input_0_and_offset(input_0_and_offset_b),
+    .input_1_and_offset(input_1_and_offset_b),
+    .input_2_and_offset(input_2_and_offset_b),
+    .input_3_and_offset(input_3_and_offset_b),
+    .read_data_valid(flag_input_buffer_read_valid_b),
+    .read_empty(input_buffer_read_empty_b),
+    .count(input_buffer_count_b)
+  );
+
+  // <--- Filter/Weight Buffer Banks --->
+  reg  [FILTER_BANKS-1:0] flag_filter_buffer_clear;
+  reg  [FILTER_BANKS-1:0] flag_filter_buffer_write_en;
+  reg  [FILTER_BANKS-1:0] flag_filter_buffer_read_en;
+  reg  [31:0]             filter_buffer_write_data;
+  reg  [FILTER_BANKS-1:0] flag_filter_buffer_clear_b;
+  reg  [FILTER_BANKS-1:0] flag_filter_buffer_write_en_b;
+  reg  [FILTER_BANKS-1:0] flag_filter_buffer_read_en_b;
+  reg  [31:0]             filter_buffer_write_data_b;
+  reg  [FILTER_BANK_ADDR_W-1:0] filter_bank_wr_idx;
+  reg  [FILTER_BANK_ADDR_W-1:0] filter_bank_rd_idx;
+
+  wire [FILTER_BANKS-1:0] filter_buffer_write_full;
+  wire [FILTER_BANKS-1:0] filter_buffer_read_empty;
+  wire [FILTER_BANKS-1:0] flag_filter_buffer_read_valid;
+  wire [31:0]             filter_buffer_read_data [0:FILTER_BANKS-1];
+  wire [8:0]              filter_buffer_count [0:FILTER_BANKS-1];
+  wire [FILTER_BANKS-1:0] filter_buffer_write_full_b;
+  wire [FILTER_BANKS-1:0] filter_buffer_read_empty_b;
+  wire [FILTER_BANKS-1:0] flag_filter_buffer_read_valid_b;
+  wire [31:0]             filter_buffer_read_data_b [0:FILTER_BANKS-1];
+  wire [8:0]              filter_buffer_count_b [0:FILTER_BANKS-1];
+  wire [FILTER_BANKS-1:0] filter_bank_wr_sel;
+  wire [FILTER_BANKS-1:0] filter_bank_rd_sel;
+
+  reg [31:0] selected_filter_data;
+  reg [31:0] selected_filter_data_b;
+  integer selected_filter_bank_idx;
+
+  always @(*) begin
+    selected_filter_data = 32'b0;
+    selected_filter_data_b = 32'b0;
+    for (selected_filter_bank_idx = 0;
+         selected_filter_bank_idx < FILTER_BANKS;
+         selected_filter_bank_idx = selected_filter_bank_idx + 1) begin
+      if (filter_bank_rd_idx == selected_filter_bank_idx[FILTER_BANK_ADDR_W-1:0]) begin
+        selected_filter_data = filter_buffer_read_data[selected_filter_bank_idx];
+        selected_filter_data_b = filter_buffer_read_data_b[selected_filter_bank_idx];
+      end
+    end
+  end
+
+  assign filter_bank_wr_sel = ({{(FILTER_BANKS-1){1'b0}}, 1'b1}
+                               << filter_bank_wr_idx[FILTER_BANK_ADDR_W-1:0]);
+  assign filter_bank_rd_sel = ({{(FILTER_BANKS-1){1'b0}}, 1'b1}
+                               << filter_bank_rd_idx[FILTER_BANK_ADDR_W-1:0]);
+
+  genvar filter_bank_i;
+  generate
+    for (filter_bank_i = 0; filter_bank_i < FILTER_BANKS; filter_bank_i = filter_bank_i + 1) begin : g_filter_buffer
+      cfu_filter_buffer u_filter_buffer (
+        .clk(clk),
+        .rst(reset),
+        .clear(flag_filter_buffer_clear[filter_bank_i]),
+        .write_en(flag_filter_buffer_write_en[filter_bank_i]),
+        .write_data(filter_buffer_write_data),
+        .write_full(filter_buffer_write_full[filter_bank_i]),
+        .read_en(flag_filter_buffer_read_en[filter_bank_i]),
+        .read_data(filter_buffer_read_data[filter_bank_i]),
+        .read_data_valid(flag_filter_buffer_read_valid[filter_bank_i]),
+        .read_empty(filter_buffer_read_empty[filter_bank_i]),
+        .count(filter_buffer_count[filter_bank_i])
+      );
+
+      cfu_filter_buffer u_filter_buffer_b (
+        .clk(clk),
+        .rst(reset),
+        .clear(flag_filter_buffer_clear_b[filter_bank_i]),
+        .write_en(flag_filter_buffer_write_en_b[filter_bank_i]),
+        .write_data(filter_buffer_write_data_b),
+        .write_full(filter_buffer_write_full_b[filter_bank_i]),
+        .read_en(flag_filter_buffer_read_en_b[filter_bank_i]),
+        .read_data(filter_buffer_read_data_b[filter_bank_i]),
+        .read_data_valid(flag_filter_buffer_read_valid_b[filter_bank_i]),
+        .read_empty(filter_buffer_read_empty_b[filter_bank_i]),
+        .count(filter_buffer_count_b[filter_bank_i])
+      );
+    end
+  endgenerate
+
+  // <--- MAC --->
+  wire signed [15:0] prod_0;
+  wire signed [15:0] prod_1;
+  wire signed [15:0] prod_2;
+  wire signed [15:0] prod_3;
+  wire signed [15:0] prod_0_b;
+  wire signed [15:0] prod_1_b;
+  wire signed [15:0] prod_2_b;
+  wire signed [15:0] prod_3_b;
+  reg signed [31:0] mac_acc_0;
+  reg signed [31:0] mac_acc_1;
+  reg signed [31:0] mac_acc_2;
+  reg signed [31:0] mac_acc_3;
+
+  cfu_mac u_mac_buffer(
+    .a(selected_filter_data),  // Weights
+    .input_0_and_offset(input_0_and_offset),
+    .input_1_and_offset(input_1_and_offset),
+    .input_2_and_offset(input_2_and_offset),
+    .input_3_and_offset(input_3_and_offset),
+    .prod_0(prod_0),
+    .prod_1(prod_1),
+    .prod_2(prod_2),
+    .prod_3(prod_3)
+  );
+
+  cfu_mac u_mac_buffer_b(
+    .a(selected_filter_data_b),  // Weights lane B
+    .input_0_and_offset(input_0_and_offset_b),
+    .input_1_and_offset(input_1_and_offset_b),
+    .input_2_and_offset(input_2_and_offset_b),
+    .input_3_and_offset(input_3_and_offset_b),
+    .prod_0(prod_0_b),
+    .prod_1(prod_1_b),
+    .prod_2(prod_2_b),
+    .prod_3(prod_3_b)
+  );
+
+
   // <--- Control --->
   assign cmd_ready = ~rsp_valid && ~flag_qnt_pending; // Block new commands while quantizer result is pending
 
   always @(posedge clk) begin
 
     // Default: 
-    flag_buffer_clear <= 1'b0;
-    flag_buffer_read_en <= 1'b0;
-    flag_buffer_write_en <= 1'b0;
-    flag_buffer_clear_b <= 1'b0;
-    flag_buffer_read_en_b <= 1'b0;
-    flag_buffer_write_en_b <= 1'b0;
+    flag_input_buffer_clear <= 1'b0;
+    flag_input_buffer_read_en <= 1'b0;
+    flag_input_buffer_write_en <= 1'b0;
+    flag_input_buffer_clear_b <= 1'b0;
+    flag_input_buffer_read_en_b <= 1'b0;
+    flag_input_buffer_write_en_b <= 1'b0;
+    flag_filter_buffer_clear <= {FILTER_BANKS{1'b0}};
+    flag_filter_buffer_read_en <= {FILTER_BANKS{1'b0}};
+    flag_filter_buffer_write_en <= {FILTER_BANKS{1'b0}};
+    flag_filter_buffer_clear_b <= {FILTER_BANKS{1'b0}};
+    flag_filter_buffer_read_en_b <= {FILTER_BANKS{1'b0}};
+    flag_filter_buffer_write_en_b <= {FILTER_BANKS{1'b0}};
     flag_qnt_start <= 1'b0;
 
     if (reset) begin
@@ -182,6 +267,8 @@ module Cfu (
       mac_acc_1 <= 32'd0;
       mac_acc_2 <= 32'd0;
       mac_acc_3 <= 32'd0;
+      filter_bank_wr_idx <= {FILTER_BANK_ADDR_W{1'b0}};
+      filter_bank_rd_idx <= {FILTER_BANK_ADDR_W{1'b0}};
       // Initialize QNT registers
       qnt_bias <= 32'sd0;
       qnt_mul <= 32'sd0;
@@ -201,6 +288,7 @@ module Cfu (
         rsp_payload_outputs_0 <= qnt_out;
         rsp_valid <= 1'b1;
         flag_qnt_pending <= 1'b0;
+        filter_bank_rd_idx <= filter_bank_rd_idx + 1'b1;
       end
 
     end else if (cmd_valid) begin
@@ -221,32 +309,53 @@ module Cfu (
             `MAC_SET_OFF: begin
               mac_offset <= cmd_payload_inputs_0[8:0];
             end
-            `MAC_GET: begin
-              rsp_payload_outputs_0 <= mac_acc_0 + mac_acc_1 + mac_acc_2 + mac_acc_3;
-            end
             `MAC_SET_INPUT_VALS: begin
-              buffer_write_data <= cmd_payload_inputs_0;
-              buffer_write_data_b <= cmd_payload_inputs_1;
-              flag_buffer_write_en <= 1'b1;
-              flag_buffer_write_en_b <= 1'b1;
+              input_buffer_write_data <= cmd_payload_inputs_0;
+              input_buffer_write_data_b <= cmd_payload_inputs_1;
+              flag_input_buffer_write_en <= 1'b1;
+              flag_input_buffer_write_en_b <= 1'b1;
             end
             `MAC_ON_BUFFER: begin             
-              flag_buffer_read_en <= 1'b1;
-              flag_buffer_read_en_b <= 1'b1;
+              flag_input_buffer_read_en <= 1'b1;
+              flag_input_buffer_read_en_b <= 1'b1;
+              flag_filter_buffer_read_en <= filter_bank_rd_sel;
+              flag_filter_buffer_read_en_b <= filter_bank_rd_sel;
               // Add MAC sum to each acc
               mac_acc_0 <= mac_acc_0 + prod_0 + prod_0_b;
               mac_acc_1 <= mac_acc_1 + prod_1 + prod_1_b;
               mac_acc_2 <= mac_acc_2 + prod_2 + prod_2_b;
               mac_acc_3 <= mac_acc_3 + prod_3 + prod_3_b;
-              // Re-append the value to keep it in the buffer for multiple filter sets
-              flag_buffer_write_en <= 1'b1;
-              buffer_write_data <= buffer_read_data;
-              flag_buffer_write_en_b <= 1'b1;
-              buffer_write_data_b <= buffer_read_data_b;
+              // Re-append the values to keep them in the buffer
+              flag_input_buffer_write_en <= 1'b1;
+              input_buffer_write_data <= input_buffer_read_data;
+              flag_input_buffer_write_en_b <= 1'b1;
+              input_buffer_write_data_b <= input_buffer_read_data_b;
+              flag_filter_buffer_write_en <= filter_bank_rd_sel;
+              filter_buffer_write_data <= selected_filter_data;
+              flag_filter_buffer_write_en_b <= filter_bank_rd_sel;
+              filter_buffer_write_data_b <= selected_filter_data_b;
             end
             `MAC_CLEAR_INPUT_VALS: begin
-              flag_buffer_clear <= 1'b1;
-              flag_buffer_clear_b <= 1'b1;
+              flag_input_buffer_clear <= 1'b1;
+              flag_input_buffer_clear_b <= 1'b1;
+            end
+            `MAC_SET_FILTER_VALS: begin
+              filter_buffer_write_data <= cmd_payload_inputs_0;
+              filter_buffer_write_data_b <= cmd_payload_inputs_1;
+              flag_filter_buffer_write_en <= filter_bank_wr_sel;
+              flag_filter_buffer_write_en_b <= filter_bank_wr_sel;
+            end
+            `MAC_CLEAR_FILTER_VALS: begin
+              flag_filter_buffer_clear <= {FILTER_BANKS{1'b1}};
+              flag_filter_buffer_clear_b <= {FILTER_BANKS{1'b1}};
+              filter_bank_wr_idx <= {FILTER_BANK_ADDR_W{1'b0}};
+              filter_bank_rd_idx <= {FILTER_BANK_ADDR_W{1'b0}};
+            end
+            `MAC_NEXT_FILTER_SET: begin
+                filter_bank_wr_idx <= filter_bank_wr_idx + 1'b1;
+            end
+            `MAC_REWIND_FILTER_SET: begin
+              filter_bank_rd_idx <= {FILTER_BANK_ADDR_W{1'b0}};
             end
           endcase
         end
@@ -255,30 +364,26 @@ module Cfu (
           case (cmd_payload_function_id[9:3])
             `SET_BIAS: begin
               qnt_bias <= cmd_payload_inputs_0;
-              rsp_payload_outputs_0 <= 32'd0;
             end
             `SET_MUL_AND_SHIFT: begin
               qnt_mul <= cmd_payload_inputs_0;
               qnt_shift <= cmd_payload_inputs_1;
-              rsp_payload_outputs_0 <= 32'd0;
             end
             `SET_OFFSET: begin
               qnt_offset <= cmd_payload_inputs_0;
-              rsp_payload_outputs_0 <= 32'd0;
             end
             `SET_MIN_AND_MAX: begin
               qnt_min <= cmd_payload_inputs_0;
               qnt_max <= cmd_payload_inputs_1;
-              rsp_payload_outputs_0 <= 32'd0;
             end
             `GET: begin
               flag_qnt_start <= 1'b1;
               flag_qnt_pending <= 1'b1;
               rsp_valid <= 1'b0;
             end
-            default: rsp_payload_outputs_0 <= 32'b0;
           endcase
         end
+
 
       endcase
     end
--- proj/gen_imgc/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
+++ proj/gen_imgc/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
@@ -71,6 +71,39 @@ inline void ConvPerChannel(
   TFLITE_DCHECK_EQ(input_depth % filter_input_depth, 0);
   const int output_height = output_shape.Dims(1);
   const int output_width = output_shape.Dims(2);
+
+  CFU_MAC_CLEAR_FILTER_VALS();
+
+  // For each filter set
+  for (int out_channel = 0; out_channel < output_depth; ++out_channel) {
+    for (int filter_y = 0; filter_y < filter_height; ++filter_y) {
+      for (int filter_x = 0; filter_x < filter_width; ++filter_x) {
+
+        int in_channel = 0;
+        for (; in_channel + 7 < filter_input_depth; in_channel += 8) {
+          const int8_t* filter_ptr =
+              filter_data +
+              Offset(filter_shape, out_channel, filter_y, filter_x,
+                     in_channel);
+          uint32_t filter_val_0 = *((const uint32_t*)(filter_ptr));
+          uint32_t filter_val_1 = *((const uint32_t*)(filter_ptr + 4));
+
+          CFU_MAC_SET_FILTER_VALS(filter_val_0, filter_val_1);
+        }
+
+        // Tail loop for channel counts not divisible by 8 (e.g. first RGB layer).
+        for (; in_channel < filter_input_depth; ++in_channel) {
+          uint32_t filter_val = (uint8_t)filter_data[Offset(
+              filter_shape, out_channel, filter_y, filter_x,
+              in_channel)];
+
+          CFU_MAC_SET_FILTER_VALS(filter_val, 0);
+        }
+      }
+    }
+    CFU_MAC_NEXT_FILTER_SET();
+  }
+
   for (int batch = 0; batch < batches; ++batch) {
     for (int out_y = 0; out_y < output_height; ++out_y) {
       const int in_y_origin = (out_y * stride_height) - pad_height;
@@ -107,12 +140,12 @@ inline void ConvPerChannel(
                                                0);
               for (; in_channel + 7 < filter_input_depth;
                    in_channel += 8, input_ptr += 8) {
-                uint32_t input_val_1 = *((const uint32_t*)(input_ptr));
-                uint32_t input_val_2 = *((const uint32_t*)(input_ptr + 4));
-                CFU_MAC_SET_INPUT_VALS(input_val_1, input_val_2);
+                uint32_t input_val_0 = *((const uint32_t*)(input_ptr));
+                uint32_t input_val_1 = *((const uint32_t*)(input_ptr + 4));
+                CFU_MAC_SET_INPUT_VALS(input_val_0, input_val_1);
               }
 
-              // Tail loop for channel counts not divisible by 4 (e.g. first RGB layer).
+              // Tail loop for channel counts not divisible by 8 (e.g. first RGB layer).
               for (; in_channel < filter_input_depth;
                    ++in_channel, ++input_ptr) {
                 uint32_t input_val = static_cast<uint8_t>(*input_ptr);
@@ -121,28 +154,24 @@ inline void ConvPerChannel(
             }
           }
 
+        // Start each output pixel with the first filter set.
+        CFU_MAC_REWIND_FILTER_SET();
+
         for (int out_channel = 0; out_channel < output_depth; ++out_channel) {
+          
+          // Calculate MAC with buffer values
           CFU_MAC_CLEAR();
           for (int filter_y = 0; filter_y < filter_height; ++filter_y) {
             for (int filter_x = 0; filter_x < filter_width; ++filter_x) {
-              int in_channel = 0;
-              const int8_t* filter_ptr =
-                  filter_data +
-                  Offset(filter_shape, out_channel, filter_y, filter_x, 0);
-              for (; in_channel + 7 < filter_input_depth;
-                   in_channel += 8, filter_ptr += 8) {
-                uint32_t filter_val_1 = *((const uint32_t*)(filter_ptr));
-                uint32_t filter_val_2 = *((const uint32_t*)(filter_ptr + 4));
 
-                CFU_MAC_ON_BUFFER(filter_val_1, filter_val_2);
+              int in_channel = 0;
+              for (; in_channel + 7 < filter_input_depth; in_channel += 8) {
+                CFU_MAC_ON_BUFFER();
               }
 
-              // Tail loop for channel counts not divisible by 4 (e.g. first RGB layer).
-              for (; in_channel < filter_input_depth;
-                   ++in_channel, ++filter_ptr) {
-                uint32_t filter_val = static_cast<uint8_t>(*filter_ptr);
-
-                CFU_MAC_ON_BUFFER(filter_val, 0);
+              // Tail loop for channel counts not divisible by 8 (e.g. first RGB layer).
+              for (; in_channel < filter_input_depth; ++in_channel) {               
+                CFU_MAC_ON_BUFFER();
               }
             }
           }
@@ -151,7 +180,6 @@ inline void ConvPerChannel(
           CFU_QNT_SET_MUL_AND_SHIFT(output_multiplier[out_channel], output_shift[out_channel]);
           output_data[Offset(output_shape, batch, out_y, out_x, out_channel)] = 
               static_cast<int8_t>(CFU_QNT_GET());
-
         }
       }
     }
